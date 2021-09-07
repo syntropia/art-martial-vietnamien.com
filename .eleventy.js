ce359ca9ecd1a8f1173e5cc0b6019b3f1b246c68
Frederic Monjo <19378676+syntropia@users.noreply.github.com>
2021-09-08 01:24:50 +0200

    Added markdown plugins to allow for finer page layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,24 @@
 module.exports = function (eleventyConfig) {
 
-  eleventyConfig.setUseGitIgnore(false);
+  eleventyConfig.setUseGitIgnore(false); // Because content/css is generated, thus in .gitignore
   eleventyConfig.addPassthroughCopy("content/css/*.css");
+  eleventyConfig.addPassthroughCopy("content/images/**/*");
+  eleventyConfig.addPassthroughCopy("content/**/*.jpg");
+  eleventyConfig.addPassthroughCopy("content/**/*.png");
+
+  let markdownIt = require("markdown-it")({ html: true, typographer: true, quotes: ['«\xA0', '\xA0»', '‘', '’'] })
+      .use(require("markdown-it-attrs"))
+      .use(require("markdown-it-header-sections"))
+      .use(require("markdown-it-image-figures"), { figcaption: true });
+  eleventyConfig.setLibrary("md", markdownIt);
 
   return {
+    templateFormats: ["html", "md"],
+    htmlTemplateEngine: "njk",
     dir: {
       input: "content",
+      includes: "../layouts",
+      data: "../data",
       output: "dist",
     }
   }
