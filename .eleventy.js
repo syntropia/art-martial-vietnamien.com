496fc35f6000ae48f909244a25ba2424b3d0b335
Frederic Monjo <19378676+syntropia@users.noreply.github.com>
2025-04-14 15:20:59 +0200

    chore: upgrade eleventy v3 + other deps

--- .eleventy.js
+++ .eleventy.js
@@ -1,34 +1,32 @@
-const yaml = require("js-yaml");
+import * as yaml from "js-yaml"
+import markdownItAttrs from "markdown-it-attrs"
+import markdownItHeaderSections from "markdown-it-header-sections"
+import markdownItFootnote from "markdown-it-footnote"
+import markdownItImageFigures from "markdown-it-image-figures"
 
-module.exports = function (eleventyConfig) {
+export default function (eleventyConfig) {
+  eleventyConfig.setUseGitIgnore(false) // Because content/css is generated, thus in .gitignore
 
-  eleventyConfig.addFilter("json", function(value) {
-    return JSON.stringify(value);
-  });
+  eleventyConfig.addFilter("json", (value) => JSON.stringify(value))
 
-  eleventyConfig.addDataExtension("yml", contents => yaml.load(contents));
+  eleventyConfig.addDataExtension("yml", (contents) => yaml.load(contents))
+  eleventyConfig.addPassthroughCopy("content/**/*.(css|jpg|png|svg)")
 
-  eleventyConfig.setUseGitIgnore(false); // Because content/css is generated, thus in .gitignore
-  eleventyConfig.addPassthroughCopy("content/css/*.css");
-  eleventyConfig.addPassthroughCopy("content/images/**/*");
-  eleventyConfig.addPassthroughCopy("content/**/*.jpg");
-  eleventyConfig.addPassthroughCopy("content/**/*.png");
-
-  let markdownIt = require("markdown-it")({ html: true, typographer: true, quotes: ['«\xA0', '\xA0»', '‘', '’'] })
-      .use(require("markdown-it-attrs"))
-      .use(require("markdown-it-header-sections"))
-      .use(require("markdown-it-footnote"))
-      .use(require("markdown-it-image-figures"), { figcaption: true })
-      .use(require("markdown-it-container-pandoc"));
-  eleventyConfig.setLibrary("md", markdownIt);
+  eleventyConfig.amendLibrary("md", (md) => {
+    md.quotes = ["«\xA0", "\xA0»", "‘", "’"]
+    md.use(markdownItAttrs)
+      .use(markdownItHeaderSections)
+      .use(markdownItFootnote)
+      .use(markdownItImageFigures, { figcaption: true, lazy: true })
+  })
 
   return {
     templateFormats: ["html", "md"],
     htmlTemplateEngine: "njk",
     dir: {
       input: "content",
-      includes: "../layouts", // Relative to input
+      includes: "../layouts", // Relative to input
       output: "dist",
-    }
+    },
   }
-};
+}
