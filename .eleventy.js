202a0bb11f903eafd6bbd1a125db0502bffe8a88
Frederic Monjo <19378676+syntropia@users.noreply.github.com>
2025-04-26 17:14:19 +0200

    ci: add base url for GH Pages deployment

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ import markdownItAttrs from "markdown-it-attrs"
 import markdownItHeaderSections from "markdown-it-header-sections"
 import markdownItFootnote from "markdown-it-footnote"
 import markdownItImageFigures from "markdown-it-image-figures"
+import { HtmlBasePlugin } from "@11ty/eleventy"
 
 export default function (eleventyConfig) {
   eleventyConfig.setUseGitIgnore(false) // Because content/css is generated, thus in .gitignore
@@ -20,9 +21,12 @@ export default function (eleventyConfig) {
       .use(markdownItImageFigures, { figcaption: true, lazy: true })
   })
 
+  eleventyConfig.addPlugin(HtmlBasePlugin)
+
   return {
     templateFormats: ["html", "md"],
     htmlTemplateEngine: "njk",
+    pathPrefix: process.env.PATH_PREFIX,
     dir: {
       input: "content",
       includes: "../layouts", // Relative to input
